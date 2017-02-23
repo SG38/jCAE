d56b19c0d39ab56cf06c2f949da9d84ec745d24b
Jerome Robert <jeromerobert@gmx.com>
2017-02-23 14:05:48 +0100

    [vtk-util] port to vtk 6.3
    The vtk 6.3 Canvas class no longer load native libraries

--- vtk-util/src/org/jcae/vtk/View.java
+++ vtk-util/src/org/jcae/vtk/View.java
@@ -36,6 +36,7 @@ import vtk.vtkInteractorStyleRubberBand3D;
 import vtk.vtkInteractorStyleTrackballCamera;
 import vtk.vtkPlaneCollection;
 import vtk.vtkRenderer;
+import vtk.vtkNativeLibrary;
 
 /**
  *
@@ -55,6 +56,12 @@ public class View extends Canvas {
 		System.getProperty("org.jcae.vtk.selectOnlyVisible", "true"));
 	static
 	{
+		vtkNativeLibrary.LoadNativeLibraries(
+			vtkNativeLibrary.VTKRENDERINGCORE,
+			vtkNativeLibrary.VTKRENDERINGOPENGL,
+			vtkNativeLibrary.VTKRENDERINGANNOTATION,
+			vtkNativeLibrary.VTKINTERACTIONWIDGETS,
+			vtkNativeLibrary.VTKINTERACTIONSTYLE);
 		Utils.setVTKLogFile(new File(System.getProperty("user.home"),
 			".vtk.log").getPath());
 		VTKMemoryManager.GC.SetDebug(Boolean.getBoolean("vtk.gc.debug"));
